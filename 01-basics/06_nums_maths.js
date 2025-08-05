8a4f8bda00aa254126733607387396c1e8141068
merafiulhoque <me.rafiulhoque@gmail.com>
2025-08-05 14:36:18 +0000

    ..

---
+++ 01-basics/06_nums_maths.js
@@ -0,0 +1,32 @@
+// const score = 400
+
+// const balance = new Number(100)
+
+// console.log(score);
+// console.log(balance);
+
+// console.log(balance.toString())
+// console.log(typeof balance.toString())
+
+// console.log(balance.toString().length);
+
+
+// const a = 22.33654525
+// console.log(a.toFixed(2))
+
+// console.log(a.toPrecision(5));
+
+// const b = 10000000000
+
+// console.log(b.toLocaleString('en-IN'))
+
+//*********************Maths********************/
+console.log(Math)
+console.log(Math.abs(-4));
+console.log(Math.round(6.57));
+console.log(Math.ceil(4.3));
+console.log(Math.floor(7.9))
+console.log(Math.min(5,2,6,4,5,4,8,4,2,0,5))
+console.log(Math.max(5,2,6,4,5,4,8,4,2,0,5))
+console.log(Math.random()*10);
+
