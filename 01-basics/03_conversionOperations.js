e9a03d24fae087c4df6892407fad6932a5120c28
merafiulhoque <me.rafiulhoque@gmail.com>
2025-08-05 03:56:42 +0000

    operations

--- 01-basics/03_conversionOperations.js
+++ 01-basics/03_conversionOperations.js
@@ -22,3 +22,33 @@ let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
 
+//*********************Operations *******************/
+
+let value = 9
+let negvalue =-value
+console.log(negvalue)
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**3)
+console.log(13/3)
+console.log(8%3)
+
+
+let str1 = "hello"
+let str2 = 'Rafiul'
+
+console.log(str1 +' '+ str2);
+
+let x = 3;
+let y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+let b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
