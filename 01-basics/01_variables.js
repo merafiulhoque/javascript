4b7372eace979a55de1c53a5c85595a00b762abc
merafiulhoque <me.rafiulhoque@gmail.com>
2025-08-05 03:06:33 +0000

    datatypes

---
+++ 01-basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail ="me.rafiulhoque@gmail.com"
+var accountPassword ="12345678"
+accountCity ="Kolkata"
+let accoutnState;
+// accountId = 255
+console.log(accountId);
+
+accountEmail = "hhc@gmil.com"
+accountPassword ="12365489652445"
+accountCity = "Delhi"
+
+// console.log(accountEmail);
+console.table([accountId, accountEmail, accountPassword, accountCity,accoutnState])
+
+/*
+Prefer not to use var keyword,,, functional script errors happens regularly.
+*/
---
+++ 01-basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"  //treat all js codes as new version
+//alert(3+3) ,, we are using node js , not browser
+
+console.log(3+3)
+console.log("Rafiul")
+
+//data types
+let name = "rafiul"
+let age = 20
+let loogedIn = true
+
+// number 2 to the power of 53
+// bigint
+//string = " "
+//boolean = true , false
+//null = standalone value
+//undefine
+//symbol = unique ness 
+//object
+
+console.log(typeof undefined);  // undefined
+console.log(typeof null);  // object
+
