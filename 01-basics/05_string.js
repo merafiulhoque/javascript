735f987f8471d1c60316716b3883421f84d0f85f
merafiulhoque <me.rafiulhoque@gmail.com>
2025-08-05 19:37:52 +0530

    Update 05_string.js

--- 01-basics/05_string.js
+++ 01-basics/05_string.js
@@ -20,3 +20,13 @@ const str1 = '     rafiul     '
 console.log(str1)
 console.log(str1.trim());
 
+const url = "https://www.rafiul.com/rafiul%20hoque"
+
+console.log(url.replace('%20',"_"))
+console.log(url.includes('her'))
+console.log(url.includes('rafiul'))
+
+const str2 = "rafiul_hoque-is-good"
+
+console.log(str2.split("_"))
+console.log(str2.split("-"))
