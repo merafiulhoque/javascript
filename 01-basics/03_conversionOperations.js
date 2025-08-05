33005e6f16fc28104c4e65b21f2133ad0dae6caa
merafiulhoque <me.rafiulhoque@gmail.com>
2025-08-05 03:29:58 +0000

    conversion

---
+++ 01-basics/03_conversionOperations.js
@@ -0,0 +1,24 @@
+let score = true
+console.log(score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(valueInNumber);
+console.log(typeof valueInNumber);
+
+
+let isLooggedIn =1
+let booleanIsLoggedIn = Boolean(isLooggedIn)
+
+console.log(isLooggedIn);
+console.log(booleanIsLoggedIn)
+
+console.log(typeof isLooggedIn)
+console.log(typeof booleanIsLoggedIn)
+
+let someNumber = 20
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
