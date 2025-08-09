a630cb50ed585c05677e0ebfbb94cce6ea776dc8
merafiulhoque <me.rafiulhoque@gmail.com>
2025-08-09 02:31:48 +0000

    object

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -5,23 +5,39 @@
 
 const mySym = Symbol("key1")
 const jsuser = {
-    name: "Xzy",
-    "fullname":"XYZ pqr",
+    name: "Rafiul",
+    "fullname":"Rafiul Hoque",
     age: 32,
     sex: "M",
-    email: "xyz@gmail.com",
+    email: "rafi@gmail.com",
     isLoggedIn: false,
     lastLoggedIn: ["monday","thursday"],
     [mySym]: "MYSYMBOL!"
 }
 
+
 // console.log(jsuser);
-console.log(jsuser.isLoggedIn);
-console.log(jsuser["sex"])
-console.log(jsuser["fullname"]);
-console.log(typeof jsuser[mySym]);
+// console.log(jsuser.isLoggedIn);
+// console.log(jsuser["sex"])
+// console.log(jsuser["fullname"]);
+// console.log(jsuser[mySym]);
 
 jsuser.age = 24
-Object.freeze(jsuser)
+jsuser.email = "pqr@gmail.com"
+// Object.freeze(jsuser)
+jsuser.email = "abc@google.com"
 jsuser.age = 30
-console.log(jsuser);
+// console.log(jsuser);
+
+jsuser.greeting = function(){
+    console.log("Hello JS users!!!!")
+}
+
+console.log(jsuser.greeting());
+console.log(jsuser.greeting);
+
+jsuser.greeting2 = function(){
+    console.log(`Hello ${this["fullname"]} , welcome onboard!!!!!`) // "this" is used for same object reference
+}
+
+console.log(jsuser.greeting2());
